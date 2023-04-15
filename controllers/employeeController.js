f0528afa6f6290c49f4601066b12191f7e4cd9fe
Payman <115703785+PeymanFakoori@users.noreply.github.com>
2023-04-15 15:49:39 +0330

    employee done

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,2 +1,91 @@
 const createError = require("http-errors");
 const employee = require("../models/EmployeeModel");
+
+const creatEmployee = async (req, res, next) => {
+  try {
+    const newEmployee = new employee({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      gender: req.body.gender,
+      dateOfBirth: req.body.dateOfBirth,
+      phone: req.body.phone,
+      nationalId: req.body.nationalId,
+      state: req.body.state,
+      company: req.body.company,
+      role: req.body.role,
+    });
+
+    const savedEmployee = await newEmployee.save();
+    const populatedEmployee = await savedEmployee.populate({
+      path: "company",
+      select: { __v: 0 },
+    });
+    const result = await res.json(populatedEmployee);
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
+};
+
+const readEmployee = async (req, res, next) => {
+  try {
+    const allEmployee = await employee.find({});
+    res.send(allEmployee);
+  } catch (error) {
+    next(createError(500, "somthing went wrong"));
+  }
+};
+
+const findEmployeeById = async (req, res, next) => {
+  try {
+    const employeeID = req.params.id;
+    const found = await employee
+      .findById(employeeID)
+      .populate({ path: "company", select: { __v: 0 } });
+    res.send(found);
+  } catch (error) {
+    console.log(error);
+    next(createError(500, "somthing went wrong"));
+  }
+};
+
+const updateEmployee = async (req, res, next) => {
+  try {
+    const fields = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      gender: req.body.gender,
+      dateOfBirth: req.body.dateOfBirth,
+      phone: req.body.phone,
+      nationalId: req.body.nationalId,
+      state: req.body.state,
+      company: req.body.company,
+      role: req.body.role,
+    };
+    const employeeID = req.params.id;
+    const findEmployee = await employee.findByIdAndUpdate(employeeID, fields, {
+      new: true,
+    });
+    res.json(findEmployee);
+  } catch (error) {
+    console.log(error);
+    return next(createError(500, error.message));
+  }
+};
+
+const removeEmployee = async (req, res, next) => {
+  try {
+    const employeeID = req.params.id;
+    const removedEmployee = await employee.findByIdAndDelete(employeeID);
+    res.json(removedEmployee);
+  } catch (error) {
+    next(createError(500, "somthing went wrong"));
+  }
+};
+
+module.exports = {
+  creatEmployee,
+  readEmployee,
+  findEmployeeById,
+  updateEmployee,
+  removeEmployee,
+};
--- routes/employeeRout.js
+++ routes/employeeRout.js
@@ -1,2 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const router = express.Router();
+
+const creationValidator = require("../middlewares/employeeValidator");
+
+const {
+  creatEmployee,
+  readEmployee,
+  findEmployeeById,
+  updateEmployee,
+  removeEmployee,
+} = require("../controllers/employeeController");
+
+router.post("/", creationValidator, creatEmployee);
+
+router.get("/all", readEmployee);
+
+router.get("/:id", findEmployeeById);
+
+router.put("/update/:id", updateEmployee);
+
+router.delete("/remove/:id", removeEmployee);
+
+module.exports = router;
